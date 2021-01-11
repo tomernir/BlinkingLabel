acba1f34a8c93621adaa545b63c7b46b788695ad
tomern <tomern@jfrog.com>
2021-01-11 11:38:05 +0200

    Initial commit

---
+++ Example/BlinkingLabel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BlinkingLabel
+//
+//  Created by tomern on 01/11/2021.
+//  Copyright (c) 2021 tomern. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
